f07988849fd71f695c7a4afcab5d10d6a0a63bea
Pratishtha Verma <pratishthaverma2000@gmail.com>
2024-11-08 00:07:32 +0530

    done

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,24 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     rollupOptions: {
+//       external: ["react", "react-dom"], // Add your external dependencies here
+//     },
+//   },
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ["react", "react-dom"], // Add your external dependencies here
+      external: ["react-router-dom"], // Explicitly mark it as external if needed
     },
   },
 });
