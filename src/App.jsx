4c3660fd910613b2ccf1b5e514ed9bc1f8ede39c
Pratishtha Verma <pratishthaverma2000@gmail.com>
2024-11-08 00:37:32 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -11,16 +11,14 @@ import TermsAndConditionsModal from "./components/TermsAndConditionsModal";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        {/* Redirect root path ("/") to "/home" */}
-        <Route path="/" element={<Navigate to="/home" />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/contact" element={<ContactForm />} />
-        {/* Uncomment this line if you need the Terms and Conditions modal */}
-        {/* <Route path="/contact/terms-conditions" element={<TermsAndConditionsModal />} /> */}
-      </Routes>
-    </Router>
+    <Routes>
+      {/* Redirect root path ("/") to "/home" */}
+      <Route path="/" element={<Navigate to="/home" />} />
+      <Route path="/home" element={<HomePage />} />
+      <Route path="/contact" element={<ContactForm />} />
+      {/* Uncomment this line if you need the Terms and Conditions modal */}
+      {/* <Route path="/contact/terms-conditions" element={<TermsAndConditionsModal />} /> */}
+    </Routes>
   );
 }
 
