89c783daf50984db511eb1b441d1c5cada51d41a
Pratishtha Verma <pratishthaverma2000@gmail.com>
2024-11-08 00:44:56 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import "./App.css";
-import { Routes, Route, Navigate } from "node_modules/react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import ContactForm from "./components/ContactForm";
 import HomePage from "./pages/HomePage";
 import TermsAndConditionsModal from "./components/TermsAndConditionsModal";
@@ -8,8 +13,8 @@ function App() {
   return (
     <Routes>
       {/* Redirect root path ("/") to "/home" */}
-      <Route path="/" element={<Navigate to="/home" />} />
-      <Route path="/home" element={<HomePage />} />
+      {/* <Route path="/" element={<Navigate to="/home" />} /> */}
+      <Route path="/" element={<HomePage />} />
       <Route path="/contact" element={<ContactForm />} />
       {/* Uncomment this line if you need the Terms and Conditions modal */}
       {/* <Route path="/contact/terms-conditions" element={<TermsAndConditionsModal />} /> */}
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 // import {} from "useRoutes";
-import { BrowserRouter } from "node_modules/react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
