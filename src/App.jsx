1dda0fe1b1d6b34b2346f8f986aee2c098b86e30
Pratishtha Verma <pratishthaverma2000@gmail.com>
2024-11-08 00:31:04 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,26 @@
 import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import ContactForm from "./components/ContactForm";
 import HomePage from "./pages/HomePage";
-// import SheetDataViewer from "./components/SheetDataViewer";
-// import SheetPage from "./pages/SheetPage";
-import { Routes, Route, Navigate } from "react-router-dom";
 import TermsAndConditionsModal from "./components/TermsAndConditionsModal";
-// import FirstPage from "./components/HomePage/FirstPage";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <Router>
       <Routes>
-        <Route path="/">
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/contact" element={<ContactForm />} />
-        </Route>
-        {/* <Route
-          path="/contact/terms-conditions"
-          element={<TermsAndConditionsModal />}
-        /> */}
-        {/* <Route path="/contact" element={<ContactForm />} /> */}
+        {/* Redirect root path ("/") to "/home" */}
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/contact" element={<ContactForm />} />
+        {/* Uncomment this line if you need the Terms and Conditions modal */}
+        {/* <Route path="/contact/terms-conditions" element={<TermsAndConditionsModal />} /> */}
       </Routes>
-      {/* <ContactForm /> */}
-      {/* <HomePage /> */}
-    </>
+    </Router>
   );
 }
 
