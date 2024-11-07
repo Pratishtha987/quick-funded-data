d0b3a0acdeae6a22e17939156a116e032c286583
Pratishtha Verma <pratishthaverma2000@gmail.com>
2024-11-08 00:52:05 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -15,11 +15,12 @@ function App() {
       <Routes>
         {/* Redirect root path ("/") to "/home" */}
         {/* <Route path="/" element={<Navigate to="/home" />} /> */}
-        <Route path="/" element={<HomePage />} />
+        {/* <Route path="/" element={<HomePage />} /> */}
         <Route path="/contact" element={<ContactForm />} />
         {/* Uncomment this line if you need the Terms and Conditions modal */}
         {/* <Route path="/contact/terms-conditions" element={<TermsAndConditionsModal />} /> */}
       </Routes>
+      <HomePage />
     </Router>
   );
 }
