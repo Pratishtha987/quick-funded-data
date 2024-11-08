bb1c1167812857221914deb9064732aab25c227e
Pratishtha Verma <pratishthaverma2000@gmail.com>
2024-11-08 14:46:24 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,17 @@
 import "./App.css";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "build.rollupOptions.external";
 import ContactForm from "./components/ContactForm";
 import HomePage from "./pages/HomePage";
+import { Routes, Route, Navigate } from "react-router-dom";
 import TermsAndConditionsModal from "./components/TermsAndConditionsModal";
 
 function App() {
   return (
-    <div className="app">
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/contact" element={<ContactForm />} />
-        </Routes>
-      </Router>
+    <div>
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/contact" element={<ContactForm />} />
+      </Routes>
     </div>
   );
 }
