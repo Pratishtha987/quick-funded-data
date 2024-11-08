04195332e3d1532e349ba5071b291d265522619c
Pratishtha Verma <pratishthaverma2000@gmail.com>
2024-11-08 15:13:46 +0530

    done

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,19 @@
 //   },
 // });
 
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     rollupOptions: {
+//       external: ["lucide-react"],
+//       external: ["react-router-dom"], // Explicitly mark it as external if needed
+//     },
+//   },
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -18,8 +31,9 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ["lucide-react"],
-      external: ["react-router-dom"], // Explicitly mark it as external if needed
+      external: ["lucide-react", "react-router-dom"],
+      // external: ["lucide-react"],
+      // external: ["react-router-dom"], // Explicitly mark it as external if needed
     },
   },
 });
